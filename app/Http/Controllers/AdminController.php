74b9e44d4e5b4da11f3ed74da15a0f9663b91130
himanshucodesoftic <himanshudhiman@codesoftic.com>
2021-03-03 19:11:20 +0530

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use DB as DBraw;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Redirect;
+// use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AdminController extends Controller
+{
+    public function index() {
+        return view('registration');
+    }
+
+
+
+    public function userPostRegistration(Request $request) {
+// dd($request);
+        // validate form fields
+        $request->validate([
+                'name'        =>             'required',
+                'email'             =>      'required|unique:users|email',
+                'password'          =>      'required',
+                'confirmpassword'  =>      'required|same:password',
+                // 'username'=>'required'
+                
+                ]);
+
+        $input          =           $request->all();
+       
+        // if validation success then create an input array
+        $inputArray      =           array(
+            'username'=> $request->txt_username,
+            'name'        =>      $request->name,
+            'email'         =>      $request->email,
+            'password'          =>      Hash::make($request->password),
+            'confirmpassword'             =>      $request->confirmpassword,
+            'city'=>$request->city,
+            'state'=>$request->state
+             
+        );
+        // dd($inputArray );
+        
+        // register user
+        $user           =           User::create($inputArray);
+        // dd($inputArray);
+        // if registration success then return with success message
+        if(!is_null($user)) {
+            // dd($user);
+            return redirect('/user-login');
+            return back()->with('success', 'You have registered successfully.');
+          
+        }
+        // else return with error message
+        else {
+            return back()->with('error', 'Whoops! some error encountered. Please try again.');
+        }
+    }
+
+    function check(Request $request)
+   	{
+   		if($request->get('username'))
+   		{
+   			$username = $request->get('username');
+   			$data = DB::table("users")
+   				->where('username', $username)
+   				->count();
+   			if($data > 0)
+   			{
+   				echo 'not_unique';
+   			}
+   			else
+   			{
+   				echo 'unique';
+   			}
+   		}
+   	}
+
+
+// -------------------- [ User login view ] -----------------------
+    public function userLoginIndex() {
+        return view('login');
+    }
+
+
+// --------------------- [ User login ] ---------------------
+    public function userPostLogin(Request $request,$id) {
+        // dd($request);
+
+        $request->validate([
+            "email"           =>    "required|email",
+            "password"        =>    "required"
+        ]);
+      
+
+        $userCredentials = $request->only('email','password');
+       
+        // check user using auth function
+
+        if (Auth::attempt($userCredentials)) {
+        
+            return redirect()->intended('dashboard');
+            
+         }
+       
+
+        else {
+            return back()->withSuccess('Whoops! invalid username or password.');
+        }
+        
+    }
+
+
+// ------------------ [ User Dashboard Section ] ---------------------
+    public function dashboard(Request $request) {
+        // check if user logged in
+        if(Auth::check()) {
+            $city = Auth::user()->city;
+            $state = Auth::user()->state;
+
+
+
+            $sel_query = "SELECT * from users";
+            $res_query = DBraw::select($sel_query);
+            $res_query = json_decode(json_encode($res_query), true);
+            if (count($res_query)) {
+                foreach ($res_query as $res) {
+    
+                
+                    $productlist = $res['id'];
+                }
+            } else {
+                $productlist = array();
+            }
+            return view('dashboard',compact("city","state",'productlist'));
+        }
+
+        return redirect::to("user-login")->withSuccess('Oopps! You do not have access');
+    }
+
+
+// ------------------- [ User logout function ] ----------------------
+public function logout(Request $request ) {
+    $request->session()->flush();
+    Auth::logout();
+    return Redirect('user-login');
+    }
+
+}
